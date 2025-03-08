2c58c8e889f1032a1aae8fa628edaa5d0066ecb3
Harsh singh baghel <harshsingh231075@gmail.com>
2025-03-08 19:58:09 +0530

    Update app.js

--- bakend/src/app.js
+++ bakend/src/app.js
@@ -5,7 +5,11 @@ const app = express();
 
 
 
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allows requests from any origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
